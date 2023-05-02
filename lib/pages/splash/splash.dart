eea000263c7ceb87bff28d6222034d751d681c34
GLinBoy <hojjatabedie@gmail.com>
2023-05-02 21:16:32 +0200

    👌 IMPROVE: Created the SplashPage Widget
    #10

--- lib/pages/splash/splash.dart
+++ lib/pages/splash/splash.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 
+const Color mainColor = Color(0xFFFF5656);
+
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go Back!'),
-        ),
+    return Container(
+      color: mainColor,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Icon(
+              Icons.terrain,
+              color: Colors.white,
+              size: 90,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.only(
+                bottom: 80,
+              ),
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
