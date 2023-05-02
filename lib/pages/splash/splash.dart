7ab1e036c425b73cf065d3bac2bb70c8b1638ce4
GLinBoy <hojjatabedie@gmail.com>
2023-05-02 21:20:05 +0200

    👌 IMPROVE: Create Dummy Landing Page
    #10

--- lib/pages/splash/splash.dart
+++ lib/pages/splash/splash.dart
@@ -37,3 +37,14 @@ class SplashPage extends StatelessWidget {
     );
   }
 }
+
+class MountsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome to Mounts of the World'),
+      ),
+    )
+  }
+}
