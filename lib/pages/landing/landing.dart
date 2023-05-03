f0c511e1f1ea9b02a43c50a99dd6517c1fe7d8e6
GLinBoy <hojjatabedie@gmail.com>
2023-05-03 20:24:34 +0200

    👌 IMPROVE: Created the app bar
    #11

--- lib/pages/landing/landing.dart
+++ lib/pages/landing/landing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/splash/splash.dart';
 
 class SplashPage extends StatelessWidget {
   SplashPage({ super.key })
@@ -7,7 +8,17 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Center(
+          child: Icon(Icons.terrain, color: mainColor, size: 40,),
+        ),
+        actions: [
+          SizedBox(width: 40, height: 40,)
+        ],
+        iconTheme: IconThemeData(color: mainColor),
+      ),
       drawer: Drawer(),
       body: Center(
         child: Text('Welcome to Mounts of the World!'),
