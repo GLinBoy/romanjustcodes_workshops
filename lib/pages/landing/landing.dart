695bd6291e89a74e18bf17499969e05c747f1031
GLinBoy <hojjatabedie@gmail.com>
2023-05-03 21:09:01 +0200

    👌 IMPROVE: Created app search
    #11

--- lib/pages/landing/landing.dart
+++ lib/pages/landing/landing.dart
@@ -42,6 +42,7 @@ class LandingPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AppHeader(),
+          AppSearch(),
         ],
       ),
     );
@@ -81,6 +82,71 @@ class AppHeader extends StatelessWidget {
                 style: TextStyle(
                   color: mainColor,
                   fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AppSearch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Discover',
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 25,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 50,
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Search',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10),
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: mainColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(
+                  Icons.tune,
+                  color: Colors.white,
                 ),
               ),
             ],
