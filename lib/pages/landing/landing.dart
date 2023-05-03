183adeda5ea625fae3fa47915f4f4b831c63a1c8
GLinBoy <hojjatabedie@gmail.com>
2023-05-03 20:15:57 +0200

    📦 NEW: Landing page has been added
    #11

---
+++ lib/pages/landing/landing.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  SplashPage({ super.key })
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
