ac21409b3797938e0dfaea0e6df36017d8625a2d
GLinBoy <hojjatabedie@gmail.com>
2023-05-04 19:47:08 +0200

    📦 NEW: Initiated detail page
    #12

---
+++ lib/pages/detail/detail.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
