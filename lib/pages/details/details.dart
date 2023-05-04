fed53a906855dc38f2a17aa573ec6868d2931ce9
GLinBoy <hojjatabedie@gmail.com>
2023-05-04 20:22:25 +0200

    👌 IMPROVE: Top region of detail page developed
    #12

--- lib/pages/details/details.dart
+++ lib/pages/details/details.dart
@@ -1,16 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/landing/landing.dart';
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var selectedItem = mountItems[3];
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
         children: [
           Expanded(
-            child: Text('Details Top'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40)),
+              child: Stack(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(30),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(selectedItem.path),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.transparent,
+                            Colors.black.withOpacity(0.7),
+                          ],
+                          begin: Alignment.center,
+                          end: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 30,
+                    left: 30,
+                    right: 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          selectedItem.name,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          selectedItem.location,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  AppBar(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    iconTheme: IconThemeData(color: Colors.white),
+                    title: Center(
+                      child: Icon(
+                        Icons.terrain,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                    actions: [
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                        child: Icon(
+                          Icons.pending,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
           Expanded(
             child: Text('Details Bottom'),
