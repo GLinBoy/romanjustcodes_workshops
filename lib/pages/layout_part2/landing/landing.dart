482f7fa037d2fae76df1820572ccec242c746d32
GLinBoy <hojjatabedie@gmail.com>
2023-05-08 20:01:22 +0200

    👌 IMPROVE: Populated the body of the header
    #13

--- lib/pages/layout_part2/landing/landing.dart
+++ lib/pages/layout_part2/landing/landing.dart
@@ -56,7 +56,86 @@ class LandingPage extends StatelessWidget {
             end: Alignment.bottomCenter,
           ),
         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            HeaderWidget(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class HeaderWidget extends StatelessWidget {
+  const HeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(
+            top: 30,
+            left: 30,
+            right: 30,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Where do you',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'want to go?',
+                style: TextStyle(
+                  color: mainYellow,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            left: 30,
+            right: 30,
+          ),
+          child: Container(
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: lightGray,
+            ),
+            margin: EdgeInsets.only(
+              top: 20,
+              bottom: 20,
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'Search',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
