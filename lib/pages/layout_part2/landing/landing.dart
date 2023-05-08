5ffc593aa181e5ede3c226648b409dff4159fd4c
GLinBoy <hojjatabedie@gmail.com>
2023-05-08 19:34:48 +0200

    👌 IMPROVE: Created the LandingPage widget
    #13

--- lib/pages/layout_part2/landing/landing.dart
+++ lib/pages/layout_part2/landing/landing.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
+final Color mainYellow = Color(0xFFFFB02F);
+final Color primaryGray = Color(0xFF313131);
+final Color secondaryGray = Color(0xFF1C1C1C);
+final Color lightGray = Color(0xFF3B3B3B);
+
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              primaryGray,
+              secondaryGray,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+      ),
+    );
   }
 }
