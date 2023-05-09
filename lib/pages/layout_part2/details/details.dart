3f8f48aa3d96b1a5d0e08f2f3d27f0bf924c7e43
GLinBoy <hojjatabedie@gmail.com>
2023-05-09 18:45:25 +0200

    📦 NEW: Added details page of part 2
    #14

---
+++ lib/pages/layout_part2/details/details.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
