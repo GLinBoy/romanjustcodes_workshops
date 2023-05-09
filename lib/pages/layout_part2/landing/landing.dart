81a11d9583bd96509afa4347d5abe7fce5b3ff1d
GLinBoy <hojjatabedie@gmail.com>
2023-05-09 18:37:23 +0200

    👌 IMPROVE: Populated BottomBar
    #13

--- lib/pages/layout_part2/landing/landing.dart
+++ lib/pages/layout_part2/landing/landing.dart
@@ -36,6 +36,13 @@ final List<AttractionModel> attractions = [
           'The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.')
 ];
 
+final List<BottomBarModel> bottomBarListItems = [
+  BottomBarModel(icon: Icons.explore_outlined, isSelected: true),
+  BottomBarModel(icon: Icons.favorite_border, isSelected: false),
+  BottomBarModel(icon: Icons.comment_outlined, isSelected: false),
+  BottomBarModel(icon: Icons.account_circle_outlined, isSelected: false),
+];
+
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
 
@@ -92,6 +99,7 @@ class LandingPage extends StatelessWidget {
           children: [
             HeaderWidget(),
             AttractionListView(),
+            BottomBarWidget(),
           ],
         ),
       ),
@@ -260,6 +268,46 @@ class AttractionCard extends StatelessWidget {
   }
 }
 
+class BottomBarWidget extends StatefulWidget {
+  @override
+  State<BottomBarWidget> createState() => BottomBarWidgetState();
+}
+
+class BottomBarWidgetState extends State<BottomBarWidget> {
+  List<BottomBarModel> _bottomBarItems = bottomBarListItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20, bottom: 20),
+      padding: EdgeInsets.all(20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: List.generate(
+          _bottomBarItems.length,
+          (index) {
+            var barItemWidget = _bottomBarItems[index];
+            return IconButton(
+              icon: Icon(
+                barItemWidget.icon,
+                color: barItemWidget.isSelected! ? mainYellow : Colors.grey,
+              ),
+              onPressed: () {
+                setState(() {
+                  _bottomBarItems.forEach((element) {
+                    element.isSelected = element == barItemWidget;
+                  });
+                });
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
 class AttractionModel {
   String? imgPath;
   String? name;
@@ -272,4 +320,11 @@ class AttractionModel {
     this.location,
     this.description,
   });
+}
+
+class BottomBarModel {
+  IconData? icon;
+  bool? isSelected;
+
+  BottomBarModel({this.icon, this.isSelected});
 }
