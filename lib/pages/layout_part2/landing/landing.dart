3be3dcb923e21e94cb30407f2c9f787d5459e73e
GLinBoy <hojjatabedie@gmail.com>
2023-05-09 19:06:48 +0200

    👌 IMPROVE: Brought it all together

--- lib/pages/layout_part2/landing/landing.dart
+++ lib/pages/layout_part2/landing/landing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/layout_part2/details/details.dart';
 
 final Color mainYellow = Color(0xFFFFB02F);
 final Color primaryGray = Color(0xFF313131);
@@ -63,11 +64,12 @@ class LandingPage extends StatelessWidget {
           Container(
             margin: EdgeInsets.only(right: 10),
             child: IconButton(
-                icon: Icon(
-                  Icons.notifications_on_outlined,
-                  color: Colors.grey,
-                ),
-                onPressed: () {}),
+              icon: Icon(
+                Icons.notifications_on_outlined,
+                color: Colors.grey,
+              ),
+              onPressed: () {},
+            ),
           ),
         ],
       ),
@@ -206,62 +208,70 @@ class AttractionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 180,
-      margin: EdgeInsets.all(10),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(25),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(attractionModel!.imgPath!),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.transparent,
-                    Colors.black.withOpacity(0.5),
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        attractionModel!.name!,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        attractionModel!.location!,
-                        style: TextStyle(color: mainYellow),
-                      ),
-                    ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => DetailsPage(
+                  selectedModel: attractionModel,
+                )));
+      },
+      child: Container(
+        width: 180,
+        margin: EdgeInsets.all(10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(25),
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(attractionModel!.imgPath!),
+                    fit: BoxFit.cover,
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.transparent,
+                      Colors.black.withOpacity(0.5),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          attractionModel!.name!,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          attractionModel!.location!,
+                          style: TextStyle(color: mainYellow),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
