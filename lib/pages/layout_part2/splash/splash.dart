1f406a55fae6833257e6cb2a285ad2b116ebc46c
GLinBoy <hojjatabedie@gmail.com>
2023-05-09 19:15:26 +0200

    👌 IMPROVE: Created the Splash Page
    #15

--- lib/pages/layout_part2/splash/splash.dart
+++ lib/pages/layout_part2/splash/splash.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/layout_part2/landing/landing.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Stack(
+      children: [
+        Container(
+          color: mainYellow,
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Icon(
+            Icons.airplanemode_on,
+            color: Colors.black,
+            size: 80,
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: SizedBox(
+            width: 160,
+            height: 160,
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(
+                Colors.black.withOpacity(0.2),
+              ),
+              strokeWidth: 10,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
