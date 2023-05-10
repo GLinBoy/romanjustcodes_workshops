0af564f2014af91f76205093caa2aee2fea3db38
GLinBoy <hojjatabedie@gmail.com>
2023-05-10 19:37:37 +0200

    👌 IMPROVE: Created the SplashPage Widget
    #16

--- lib/pages/layout_part3/splash/splash.dart
+++ lib/pages/layout_part3/splash/splash.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
+const mainThemeColor = Color(0xFF272D8D);
+
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack();
+    return Stack(
+      children: [
+        Container(
+          color: mainThemeColor,
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Icon(
+            Icons.pool,
+            color: Colors.white,
+            size: 80,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: LinearProgressIndicator(
+            valueColor: AlwaysStoppedAnimation(Colors.white.withOpacity(0.4)),
+          ),
+        ),
+      ],
+    );
   }
 }
