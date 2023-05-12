36d7cd2e78eee58eeb8614921ed7820f4d7db885
GLinBoy <hojjatabedie@gmail.com>
2023-05-12 21:07:45 +0200

    👌 IMPROVE: Updated path of images on layout pt.2
    #38

--- lib/pages/layout_part2/landing/landing.dart
+++ lib/pages/layout_part2/landing/landing.dart
@@ -8,29 +8,25 @@ Color lightGray = const Color(0xFF3B3B3B);
 
 final List<AttractionModel> attractions = [
   AttractionModel(
-      imgPath:
-          'https://images.pexels.com/photos/260590/pexels-photo-260590.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      imgPath: 'images/layout_part1/pexels-photo-260590.jpg',
       name: 'Golden Gate Bridge',
       location: 'San Francisco, CA',
       description:
           'The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.'),
   AttractionModel(
-      imgPath:
-          'https://images.pexels.com/photos/5627275/pexels-photo-5627275.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      imgPath: 'images/layout_part1/pexels-photo-5627275.jpg',
       name: 'Brooklyn Bridge',
       location: 'Brooklyn, NY',
       description:
           'The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.'),
   AttractionModel(
-      imgPath:
-          'https://images.pexels.com/photos/5241381/pexels-photo-5241381.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      imgPath: 'images/layout_part1/pexels-photo-5241381.jpg',
       name: 'London Bridge',
       location: 'London, UK',
       description:
           'The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.'),
   AttractionModel(
-      imgPath:
-          'https://images.pexels.com/photos/1680247/pexels-photo-1680247.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      imgPath: 'images/layout_part1/pexels-photo-1680247.jpg',
       name: 'Harbour Bridge',
       location: 'Sydney, AU',
       description:
