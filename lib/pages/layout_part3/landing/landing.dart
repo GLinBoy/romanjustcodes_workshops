a66e44660c331fd5835fdcf10aab9919bbbd2db4
GLinBoy <hojjatabedie@gmail.com>
2023-05-15 19:48:49 +0200

    👌 IMPROVE: Added search field
    #19

--- lib/pages/layout_part3/landing/landing.dart
+++ lib/pages/layout_part3/landing/landing.dart
@@ -68,6 +68,27 @@ class LandingPage extends StatelessWidget {
 class LandingSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.only(left: 30, right: 30),
+      padding: EdgeInsets.only(
+        top: 5,
+        bottom: 5,
+        left: 20,
+        right: 5,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Search hotel',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ],
+      ),
+    );
   }
 }
