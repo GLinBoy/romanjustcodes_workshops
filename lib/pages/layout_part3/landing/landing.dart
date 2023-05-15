2a5050b7f85e7ce9ae531c001d1ee551bce88c22
GLinBoy <hojjatabedie@gmail.com>
2023-05-15 19:43:40 +0200

    👌 IMPROVE: Layed Down the Content Column Widget
    #19

--- lib/pages/layout_part3/landing/landing.dart
+++ lib/pages/layout_part3/landing/landing.dart
@@ -18,6 +18,46 @@ class LandingPage extends StatelessWidget {
           Container(
             color: mainThemeColor.withOpacity(0.7),
           ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Paradise',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(height: 60),
+              Icon(
+                Icons.pool,
+                color: Colors.white,
+                size: 80,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Choose location to'.toUpperCase(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.5),
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Find a Hotel',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ],
       ),
     );
