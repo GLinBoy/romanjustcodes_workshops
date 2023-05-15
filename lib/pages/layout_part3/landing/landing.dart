6ab8ce20347d8834c9a543ed718778cc447e92b1
GLinBoy <hojjatabedie@gmail.com>
2023-05-15 19:31:41 +0200

    👌 IMPROVE: Built the body to the Landing Page Widget's Scaffold
    #19

--- lib/pages/layout_part3/landing/landing.dart
+++ lib/pages/layout_part3/landing/landing.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Landing Page');
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage('<BG_IMAGE_PATH>'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
