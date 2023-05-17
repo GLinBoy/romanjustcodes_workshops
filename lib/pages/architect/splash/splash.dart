f2f31f6c7c56796b4d2bf234dc6c27aa616a08c4
GLinBoy <hojjatabedie@gmail.com>
2023-05-17 20:02:24 +0200

    👌 IMPROVE: Created app bar on Landing page
    #35

--- lib/pages/architect/splash/splash.dart
+++ lib/pages/architect/splash/splash.dart
@@ -72,6 +72,16 @@ class DonutShopMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Utils.mainDark),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Image.network(
+          Utils.donutLogoRedText,
+          width: 120,
+        ),
+      ),
       body: Center(
         child: Text('Hello, Donut Shop!'),
       ),
