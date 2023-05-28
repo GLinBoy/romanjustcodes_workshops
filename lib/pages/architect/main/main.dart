db193521599c838f0df2af0823ac1e1da9a0d286
GLinBoy <hojjatabedie@gmail.com>
2023-05-28 13:27:55 +0200

    👌 IMPROVE: Added donut slider to the main page

--- lib/pages/architect/main/main.dart
+++ lib/pages/architect/main/main.dart
@@ -1,10 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/architect/splash/splash.dart';
 
 class DonutMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      children: [
+        DonutPager(),
+      ],
     );
   }
+}
+
+class DonutPager extends StatefulWidget {
+  @override
+  State<DonutPager> createState() => _DonutPagerState();
+}
+
+class _DonutPagerState extends State<DonutPager> {
+  List<DonutPage> pages = [
+    DonutPage(imgUrl: Utils.donutPromo1, logoImgUrl: Utils.donutLogoWhiteText),
+    DonutPage(imgUrl: Utils.donutPromo2, logoImgUrl: Utils.donutLogoWhiteText),
+    DonutPage(imgUrl: Utils.donutPromo3, logoImgUrl: Utils.donutLogoRedText),
+  ];
+
+  int currentPage = 0;
+  PageController? controller;
+
+  @override
+  void initState() {
+    controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 350,
+      child: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              scrollDirection: Axis.horizontal,
+              pageSnapping: true,
+              controller: controller,
+              children: List.generate(
+                pages.length,
+                (index) {
+                  DonutPage currentPage = pages[index];
+                  return Container(
+                    alignment: Alignment.bottomLeft,
+                    margin: EdgeInsets.all(20),
+                    padding: EdgeInsets.all(30),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 10,
+                          offset: Offset(0.0, 5.0),
+                        ),
+                      ],
+                      image: DecorationImage(
+                        image: NetworkImage(currentPage.imgUrl!),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Image.network(
+                      currentPage.logoImgUrl!,
+                      width: 120,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DonutPage {
+  String? imgUrl;
+  String? logoImgUrl;
+
+  DonutPage({this.imgUrl, this.logoImgUrl});
 }
