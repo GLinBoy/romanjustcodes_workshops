916cfa5f3861286e1af4e0320c21082a662b061e
GLinBoy <hojjatabedie@gmail.com>
2023-05-29 20:26:56 +0200

    📦 NEW: Added the DonutModel
    #35

--- lib/pages/architect/main/main.dart
+++ lib/pages/architect/main/main.dart
@@ -253,3 +253,19 @@ class DonutFilterBarItem {
 
   DonutFilterBarItem({this.id, this.label});
 }
+
+class DonutModel {
+  String? imgUrl;
+  String? name;
+  String? description;
+  double? price;
+  String? type;
+
+  DonutModel({
+    this.imgUrl,
+    this.name,
+    this.description,
+    this.price,
+    this.type,
+  });
+}
