bbbee88b4dd086f633ddb5e637a5bcfcdbd8423a
GLinBoy <hojjatabedie@gmail.com>
2023-05-29 20:30:36 +0200

    👌 IMPROVE: Added donut data to utils
    #35

--- lib/pages/architect/splash/splash.dart
+++ lib/pages/architect/splash/splash.dart
@@ -231,4 +231,128 @@ class Utils {
       'https://romanejaquez.github.io/flutter-codelab4/assets/donut_promo2.png';
   static const String donutPromo3 =
       'https://romanejaquez.github.io/flutter-codelab4/assets/donut_promo3.png';
+
+  static List<DonutModel> donuts = [
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutclassic/donut_classic1.png',
+        name: 'Strawberry Sprinkled Glazed',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 1.99,
+        type: 'classic'),
+    DonutModel(
+      imgUrl:
+          'https://romanejaquez.github.io/flutter-codelab4/assets/donutclassic/donut_classic2.png',
+      name: 'Chocolate Glazed Doughnut',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+      price: 2.99,
+      type: 'classic',
+    ),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutclassic/donut_classic3.png',
+        name: 'Chocolate Dipped Doughnut',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 2.99,
+        type: 'classic'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutclassic/donut_classic4.png',
+        name: 'Cinamon Glazed Glazed',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 2.99,
+        type: 'classic'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutclassic/donut_classic5.png',
+        name: 'Sugar Glazed Doughnut',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 1.99,
+        type: 'classic'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutsprinkled/donut_sprinkled1.png',
+        name: 'Halloween Chocolate Glazed',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 2.99,
+        type: 'sprinkled'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutsprinkled/donut_sprinkled2.png',
+        name: 'Party Sprinkled Cream',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 1.99,
+        type: 'sprinkled'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutsprinkled/donut_sprinkled3.png',
+        name: 'Chocolate Glazed Sprinkled',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 1.99,
+        type: 'sprinkled'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutsprinkled/donut_sprinkled4.png',
+        name: 'Strawbery Glazed Sprinkled',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 2.99,
+        type: 'sprinkled'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutsprinkled/donut_sprinkled5.png',
+        name: 'Reese\'s Sprinkled',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 3.99,
+        type: 'sprinkled'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutstuffed/donut_stuffed1.png',
+        name: 'Brownie Cream Doughnut',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 1.99,
+        type: 'stuffed'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutstuffed/donut_stuffed2.png',
+        name: 'Jelly Stuffed Doughnut',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 2.99,
+        type: 'stuffed'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutstuffed/donut_stuffed3.png',
+        name: 'Caramel Stuffed Doughnut',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 2.59,
+        type: 'stuffed'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutstuffed/donut_stuffed4.png',
+        name: 'Maple Stuffed Doughnut',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 1.99,
+        type: 'stuffed'),
+    DonutModel(
+        imgUrl:
+            'https://romanejaquez.github.io/flutter-codelab4/assets/donutstuffed/donut_stuffed5.png',
+        name: 'Glazed Jelly Stuffed Doughnut',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit, tellus condimentum cursus gravida, lorem augue venenatis elit, sit amet bibendum quam neque id sapien.',
+        price: 1.59,
+        type: 'stuffed')
+  ];
 }
