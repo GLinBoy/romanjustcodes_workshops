778a47b5e3a0265a729a9d6813bb3dfd302e0081
GLinBoy <hojjatabedie@gmail.com>
2023-05-30 12:18:17 +0200

    👌 IMPROVE: Applied donut filter
    #35

--- lib/pages/architect/main/main.dart
+++ lib/pages/architect/main/main.dart
@@ -9,7 +9,13 @@ class DonutMainPage extends StatelessWidget {
       children: [
         DonutPager(),
         DonutFilterBar(),
-        Expanded(child: DonutList(donuts: Utils.donuts)),
+        Expanded(
+          child: Consumer<DonutService>(
+            builder: (context, donutService, child) {
+              return DonutList(donuts: donutService.filteredDonuts);
+            },
+          ),
+        ),
       ],
     );
   }
@@ -230,13 +236,17 @@ class DonutService extends ChangeNotifier {
   ];
 
   String? selectedDonutType;
+  List<DonutModel> filteredDonuts = [];
 
   DonutService() {
     selectedDonutType = filterBarItems.first.id;
+    filteredDonutsByType(selectedDonutType!);
   }
 
   void filteredDonutsByType(String type) {
     selectedDonutType = type;
+    filteredDonuts =
+        Utils.donuts.where((d) => d.type == selectedDonutType).toList();
     notifyListeners();
   }
 }
