29262f6e13a528aa6e03adf20d14423978092ca0
GLinBoy <hojjatabedie@gmail.com>
2023-05-30 11:54:34 +0200

    👌 IMPROVE: Updated donut card return widget
    #35

--- lib/pages/architect/main/main.dart
+++ lib/pages/architect/main/main.dart
@@ -265,7 +265,33 @@ class _DonutListState extends State<DonutList> {
 class DonutCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack();
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: 150,
+          padding: EdgeInsets.all(15),
+          alignment: Alignment.bottomLeft,
+          margin: EdgeInsets.only(left: 10, top: 80, right: 10, bottom: 20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 10,
+                offset: Offset(0.0, 4.0),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+        ),
+      ],
+    );
   }
 }
 
