3814e60348cf7906dbfb4f5e59e33013aebcd827
GLinBoy <hojjatabedie@gmail.com>
2023-05-30 12:39:50 +0200

    📦 NEW: Created donut shop details

---
+++ lib/pages/architect/details/details.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DonutShopDetails extends StatefulWidget {
+  @override
+  State<DonutShopDetails> createState() => _DonutShopDetailsState();
+}
+
+class _DonutShopDetailsState extends State<DonutShopDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
