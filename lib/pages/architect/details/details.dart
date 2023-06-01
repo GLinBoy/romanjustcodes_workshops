a31ad03c26b0f77c8e36f0b9dac488824cec8b29
GLinBoy <hojjatabedie@gmail.com>
2023-06-01 19:17:46 +0200

    👌 IMPROVE: Added the details page app bar

--- lib/pages/architect/details/details.dart
+++ lib/pages/architect/details/details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/architect/splash/splash.dart';
 
 class DonutShopDetails extends StatefulWidget {
   @override
@@ -8,6 +9,16 @@ class DonutShopDetails extends StatefulWidget {
 class _DonutShopDetailsState extends State<DonutShopDetails> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Utils.mainDark),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: SizedBox(
+          width: 120,
+          child: Image.network(Utils.donutLogoRedText),
+        ),
+      ),
+    );
   }
 }
