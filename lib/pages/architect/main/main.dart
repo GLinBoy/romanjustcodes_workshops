e5b04fa7f1fbfd24a00eea5937badf5e65f33917
GLinBoy <hojjatabedie@gmail.com>
2023-06-01 20:17:56 +0200

    👌 IMPROVE: Handled navigation
    #36

--- lib/pages/architect/main/main.dart
+++ lib/pages/architect/main/main.dart
@@ -320,71 +320,77 @@ class DonutCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Container(
-          width: 150,
-          padding: EdgeInsets.all(15),
-          alignment: Alignment.bottomLeft,
-          margin: EdgeInsets.only(left: 10, top: 80, right: 10, bottom: 20),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.05),
-                blurRadius: 10,
-                offset: Offset(0.0, 4.0),
-              ),
-            ],
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '${donutInfo!.name}',
-                style: TextStyle(
-                  color: Utils.mainColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
+    return GestureDetector(
+      onTap: () {
+        var donutService = Provider.of<DonutService>(context, listen: false);
+        donutService.onDonutSelected(donutInfo!);
+      },
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: 150,
+            padding: EdgeInsets.all(15),
+            alignment: Alignment.bottomLeft,
+            margin: EdgeInsets.only(left: 10, top: 80, right: 10, bottom: 20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 10,
+                  offset: Offset(0.0, 4.0),
                 ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                  color: Utils.mainColor,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.only(
-                  left: 10,
-                  right: 10,
-                  top: 5,
-                  bottom: 5,
-                ),
-                child: Text(
-                  '\$${donutInfo!.price!.toStringAsFixed(2)}',
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${donutInfo!.name}',
                   style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white,
+                    color: Utils.mainColor,
                     fontWeight: FontWeight.bold,
+                    fontSize: 15,
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Utils.mainColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.only(
+                    left: 10,
+                    right: 10,
+                    top: 5,
+                    bottom: 5,
+                  ),
+                  child: Text(
+                    '\$${donutInfo!.price!.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Image.network(
-            donutInfo!.imgUrl!,
-            width: 150,
-            height: 150,
-            fit: BoxFit.contain,
+          Align(
+            alignment: Alignment.topCenter,
+            child: Image.network(
+              donutInfo!.imgUrl!,
+              width: 150,
+              height: 150,
+              fit: BoxFit.contain,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
