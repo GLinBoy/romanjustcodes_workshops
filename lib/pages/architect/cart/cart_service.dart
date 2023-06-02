ad3d6498767ad25011faf4593df2d6b3b254b72e
GLinBoy <hojjatabedie@gmail.com>
2023-06-02 18:52:50 +0200

    👌 IMPROVE: Added a donut list to keep our cart
    #40

--- lib/pages/architect/cart/cart_service.dart
+++ lib/pages/architect/cart/cart_service.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/architect/main/main.dart';
 
-class DonutShoppingCartService extends ChangeNotifier {}
+class DonutShoppingCartService extends ChangeNotifier {
+  List<DonutModel> cartDonuts = [];
+}
