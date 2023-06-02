4e269dafa623e222bcc5395ff308f72071074359
GLinBoy <hojjatabedie@gmail.com>
2023-06-02 18:54:34 +0200

    👌 IMPROVE: Added a method to add donut to our cart
    #40

--- lib/pages/architect/cart/cart_service.dart
+++ lib/pages/architect/cart/cart_service.dart
@@ -3,4 +3,9 @@ import 'package:romanjustcodes_workshops/pages/architect/main/main.dart';
 
 class DonutShoppingCartService extends ChangeNotifier {
   List<DonutModel> cartDonuts = [];
+
+  void addToCart(DonutModel donut) {
+    cartDonuts.add(donut);
+    notifyListeners();
+  }
 }
