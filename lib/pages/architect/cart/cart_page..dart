a6061964cad513761c6230abb4fcb64876deed83
GLinBoy <hojjatabedie@gmail.com>
2023-06-04 15:08:29 +0200

    📦 NEW: Added donut cart page
    #40

---
+++ lib/pages/architect/cart/cart_page..dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DonutShoppingCartPage extends StatefulWidget {
+  @override
+  State<DonutShoppingCartPage> createState() => _DonutShoppingCartPageState();
+}
+
+class _DonutShoppingCartPageState extends State<DonutShoppingCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
