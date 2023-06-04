c30cc0b17955130e5f7e421bb14d3708923e95ea
GLinBoy <hojjatabedie@gmail.com>
2023-06-04 15:04:54 +0200

    👌 IMPROVE: Updated cart to read value from service
    #40

--- lib/pages/architect/badge/badge.dart
+++ lib/pages/architect/badge/badge.dart
@@ -1,34 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:romanjustcodes_workshops/pages/architect/cart/cart_service.dart';
 import 'package:romanjustcodes_workshops/pages/architect/splash/splash.dart';
 
 class DonutShoppingCartBadge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(right: 10),
-      padding: EdgeInsets.only(left: 20, right: 20),
-      decoration: BoxDecoration(
-        color: Utils.mainColor,
-        borderRadius: BorderRadius.circular(40),
-      ),
-      child: Row(
-        children: [
-          Text(
-            '2',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+    return Consumer<DonutShoppingCartService>(
+      builder: (context, cartService, child) {
+        if (cartService.cartDonuts.isEmpty) {
+          return SizedBox();
+        }
+        return Transform.scale(
+          scale: 0.7,
+          child: Container(
+            margin: EdgeInsets.only(right: 10),
+            padding: EdgeInsets.only(left: 20, right: 20),
+            decoration: BoxDecoration(
+              color: Utils.mainColor,
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Row(
+              children: [
+                Text(
+                  '${cartService.cartDonuts.length}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Icon(
+                  Icons.shopping_cart,
+                  size: 25,
+                  color: Colors.white,
+                ),
+              ],
             ),
           ),
-          SizedBox(width: 10),
-          Icon(
-            Icons.shopping_cart,
-            size: 25,
-            color: Colors.white,
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
