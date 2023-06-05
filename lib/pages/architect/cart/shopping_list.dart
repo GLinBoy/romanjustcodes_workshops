75527929cfa0946521dfaa32418d03f6c542c8de
GLinBoy <hojjatabedie@gmail.com>
2023-06-05 20:31:25 +0200

    👌 IMPROVE: Updated structure of shopping list widget
    #40

--- lib/pages/architect/cart/shopping_list.dart
+++ lib/pages/architect/cart/shopping_list.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/architect/cart/shopping_list_row.dart';
+import 'package:romanjustcodes_workshops/pages/architect/main/main.dart';
 
 class DonutShoppingList extends StatefulWidget {
+  List<DonutModel>? donutCart;
+  DonutShoppingList({this.donutCart});
+
   @override
   State<DonutShoppingList> createState() => _DonutShoppingListState();
 }
@@ -8,6 +13,15 @@ class DonutShoppingList extends StatefulWidget {
 class _DonutShoppingListState extends State<DonutShoppingList> {
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return ListView.builder(
+      itemCount: widget.donutCart!.length,
+      itemBuilder: (context, index) {
+        DonutModel currentDonut = widget.donutCart![index];
+        return DonutShoppingListRow(
+          donut: currentDonut,
+          onDeleteRow: () {},
+        );
+      },
+    );
   }
 }
