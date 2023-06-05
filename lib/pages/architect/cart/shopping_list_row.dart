59b45befd1f98884bf1eb67e5b865bdee2d401ab
GLinBoy <hojjatabedie@gmail.com>
2023-06-05 19:47:04 +0200

    📦 NEW: Added donut shopping list row class
    #40

---
+++ lib/pages/architect/cart/shopping_list_row.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DonutShoppingListRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 10, right: 20),
+    );
+  }
+}
