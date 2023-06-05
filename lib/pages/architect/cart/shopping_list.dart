cc4b989fcdd8ee213216edcb7ba3740367986d79
GLinBoy <hojjatabedie@gmail.com>
2023-06-05 20:36:08 +0200

    👌 IMPROVE: Handled delete item from cart list
    #40

--- lib/pages/architect/cart/shopping_list.dart
+++ lib/pages/architect/cart/shopping_list.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/architect/cart/cart_service.dart';
 import 'package:romanjustcodes_workshops/pages/architect/cart/shopping_list_row.dart';
 import 'package:romanjustcodes_workshops/pages/architect/main/main.dart';
 
 class DonutShoppingList extends StatefulWidget {
   List<DonutModel>? donutCart;
-  DonutShoppingList({this.donutCart});
+  DonutShoppingCartService? cartService;
+
+  DonutShoppingList({this.donutCart, this.cartService});
 
   @override
   State<DonutShoppingList> createState() => _DonutShoppingListState();
@@ -19,7 +22,9 @@ class _DonutShoppingListState extends State<DonutShoppingList> {
         DonutModel currentDonut = widget.donutCart![index];
         return DonutShoppingListRow(
           donut: currentDonut,
-          onDeleteRow: () {},
+          onDeleteRow: () {
+            widget.cartService!.removeFromCart(currentDonut);
+          },
         );
       },
     );
