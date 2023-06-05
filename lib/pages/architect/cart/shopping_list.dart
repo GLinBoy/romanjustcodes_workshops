42709ba74983688b71860513ef9b7913f98d37fb
GLinBoy <hojjatabedie@gmail.com>
2023-06-05 20:00:50 +0200

    📦 NEW: Created shopping list widget for donut
    #40

---
+++ lib/pages/architect/cart/shopping_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DonutShoppingList extends StatefulWidget {
+  @override
+  State<DonutShoppingList> createState() => _DonutShoppingListState();
+}
+
+class _DonutShoppingListState extends State<DonutShoppingList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
+  }
+}
