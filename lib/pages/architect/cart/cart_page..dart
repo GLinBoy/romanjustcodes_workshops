9b51396c9759d6a947d449d6594f4990f749f1e4
GLinBoy <hojjatabedie@gmail.com>
2023-06-05 19:36:22 +0200

    👌 IMPROVE: Added and animated title to the cart page
    #40

--- lib/pages/architect/cart/cart_page..dart
+++ lib/pages/architect/cart/cart_page..dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/architect/splash/splash.dart';
 
 class DonutShoppingCartPage extends StatefulWidget {
   @override
   State<DonutShoppingCartPage> createState() => _DonutShoppingCartPageState();
 }
 
-class _DonutShoppingCartPageState extends State<DonutShoppingCartPage> {
+class _DonutShoppingCartPageState extends State<DonutShoppingCartPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController? titleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    titleAnimation = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this)
+      ..forward();
+  }
+
+  @override
+  void dispose() {
+    titleAnimation!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: EdgeInsets.all(40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          FadeTransition(
+            opacity: Tween(begin: 0.0, end: 1.0).animate(
+              CurvedAnimation(
+                parent: titleAnimation!,
+                curve: Curves.easeInOut,
+              ),
+            ),
+            child: Image.network(Utils.donutTitleMyDonuts, width: 170),
+          ),
+        ],
+      ),
+    );
   }
 }
