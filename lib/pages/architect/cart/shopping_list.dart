87ed24ee57ee7705c4d7ed42f5eb5c44c6285f41
GLinBoy <hojjatabedie@gmail.com>
2023-06-10 13:43:41 +0200

    👌 IMPROVE: Added donut shopping list items animated
    #40

--- lib/pages/architect/cart/shopping_list.dart
+++ lib/pages/architect/cart/shopping_list.dart
@@ -14,17 +14,37 @@ class DonutShoppingList extends StatefulWidget {
 }
 
 class _DonutShoppingListState extends State<DonutShoppingList> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+  List<DonutModel> insertedItems = [];
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.donutCart!.length,
-      itemBuilder: (context, index) {
+    return AnimatedList(
+      key: _key,
+      initialItemCount: insertedItems.length,
+      itemBuilder: (context, index, animation) {
         DonutModel currentDonut = widget.donutCart![index];
-        return DonutShoppingListRow(
-          donut: currentDonut,
-          onDeleteRow: () {
-            widget.cartService!.removeFromCart(currentDonut);
-          },
+        return SlideTransition(
+          position: Tween(
+            begin: const Offset(0.0, 0.2),
+            end: const Offset(0.0, 0.0),
+          ).animate(
+            CurvedAnimation(
+              parent: animation,
+              curve: Curves.easeInOut,
+            ),
+          ),
+          child: FadeTransition(
+            opacity: Tween(begin: 0.0, end: 1.0).animate(
+              CurvedAnimation(parent: animation, curve: Curves.easeInOut),
+            ),
+            child: DonutShoppingListRow(
+              donut: currentDonut,
+              onDeleteRow: () {
+                widget.cartService!.removeFromCart(currentDonut);
+              },
+            ),
+          ),
         );
       },
     );
