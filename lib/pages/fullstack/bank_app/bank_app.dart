985a65410f63aeb393583f5b600a0c2688ec5ddb
GLinBoy <hojjatabedie@gmail.com>
2023-06-12 19:59:44 +0200

    👌 IMPROVE: Updated  structure of bank app
    #41

--- lib/pages/fullstack/bank_app/bank_app.dart
+++ lib/pages/fullstack/bank_app/bank_app.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FlutterBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material();
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+    );
   }
 }
