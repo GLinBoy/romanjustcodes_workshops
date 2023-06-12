ecd7906b11a1f6a0a59957b43eaa6bcf316a9cf5
GLinBoy <hojjatabedie@gmail.com>
2023-06-12 20:00:58 +0200

    🐛 FIX: Updated class name of bank app splash
    #41

--- lib/pages/fullstack/bank_splash/bank_splash.dart
+++ lib/pages/fullstack/bank_splash/bank_splash.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-class FlutterBankApp extends StatelessWidget {
+class FlutterBankSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
