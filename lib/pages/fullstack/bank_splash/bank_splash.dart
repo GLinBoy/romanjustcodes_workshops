a12ef9d88135e540718625d100f80ca0f6ad68bd
GLinBoy <hojjatabedie@gmail.com>
2023-06-12 20:02:08 +0200

    👌 IMPROVE: Updated structure of bank app splash
    #41

--- lib/pages/fullstack/bank_splash/bank_splash.dart
+++ lib/pages/fullstack/bank_splash/bank_splash.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class FlutterBankSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFF8700C3),
+    );
   }
 }
