38de3fb0360967547fc238dbf3d6af2cc1a94947
GLinBoy <hojjatabedie@gmail.com>
2023-06-13 20:10:04 +0200

    👌 IMPROVE: Forward to login page after splash
    #41

--- lib/pages/fullstack/bank_splash/bank_splash.dart
+++ lib/pages/fullstack/bank_splash/bank_splash.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/fullstack/bank_login/bank_login.dart';
 import 'package:romanjustcodes_workshops/pages/fullstack/utils/utils.dart';
 
 class FlutterBankSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => FlutterBankLogin(),
+        ),
+      );
+    });
     return Scaffold(
       backgroundColor: Utils.mainThemeColor,
       body: Stack(
