0884c7474fe13266e74a5bd420868da3f69fc6ee
GLinBoy <hojjatabedie@gmail.com>
2023-06-13 20:35:24 +0200

    👌 IMPROVE: Added bank icon to login page
    #42

--- lib/pages/fullstack/bank_login/bank_login.dart
+++ lib/pages/fullstack/bank_login/bank_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/fullstack/utils/utils.dart';
 
 class FlutterBankLogin extends StatefulWidget {
   @override
@@ -9,8 +10,30 @@ class FlutterBankLoginState extends State<FlutterBankLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 7,
+                  color: Utils.mainThemeColor,
+                ),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: const Icon(
+                Icons.savings,
+                color: Utils.mainThemeColor,
+                size: 45,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
