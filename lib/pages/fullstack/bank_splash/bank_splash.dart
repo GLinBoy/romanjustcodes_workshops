db4ffcb1b65755d513cd12292565444ea039acf4
GLinBoy <hojjatabedie@gmail.com>
2023-06-13 19:48:49 +0200

    👌 IMPROVE: Used Utils class

--- lib/pages/fullstack/bank_splash/bank_splash.dart
+++ lib/pages/fullstack/bank_splash/bank_splash.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:romanjustcodes_workshops/pages/fullstack/utils/utils.dart';
 
 class FlutterBankSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF8700C3),
+      backgroundColor: Utils.mainThemeColor,
     );
   }
 }
