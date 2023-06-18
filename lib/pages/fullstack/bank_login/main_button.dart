e6adda57b54b8e85bae78327e79d33d06d4f73a2
GLinBoy <hojjatabedie@gmail.com>
2023-06-18 18:03:34 +0200

    👌 IMPROVE: Added field and constructor
    #42

--- lib/pages/fullstack/bank_login/main_button.dart
+++ lib/pages/fullstack/bank_login/main_button.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/widgets.dart';
 
 class FlutterBankMainButton extends StatelessWidget {
+  final Function? onTap;
+  final String? label;
+  final bool? enabled;
+
+  const FlutterBankMainButton({
+    super.key,
+    this.label,
+    this.onTap,
+    this.enabled = true,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Column();
