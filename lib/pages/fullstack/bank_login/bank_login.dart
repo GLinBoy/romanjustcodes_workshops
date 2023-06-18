7e122287accd54451a5efdae9c9f56188ff6395e
GLinBoy <hojjatabedie@gmail.com>
2023-06-18 17:48:31 +0200

    👌 IMPROVE: Added the email input for login page
    #42

--- lib/pages/fullstack/bank_login/bank_login.dart
+++ lib/pages/fullstack/bank_login/bank_login.dart
@@ -65,6 +65,38 @@ class FlutterBankLoginState extends State<FlutterBankLogin> {
                       ),
                     ),
                     const SizedBox(height: 10),
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: TextField(
+                        onChanged: (text) {
+                          setState(() {
+                            // ?
+                          });
+                        },
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon:
+                              Icon(Icons.email, color: Utils.mainThemeColor),
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.only(
+                            left: 20,
+                            bottom: 11,
+                            top: 11,
+                            right: 15,
+                          ),
+                          hintText: "Email",
+                        ),
+                        style: const TextStyle(fontSize: 16),
+                        controller: usernameController,
+                      ),
+                    )
                   ],
                 ),
               ),
