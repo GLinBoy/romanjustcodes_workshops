b3cc976b239ec008663ba4abe25646f6eb175378
GLinBoy <hojjatabedie@gmail.com>
2023-06-19 20:10:15 +0200

    👌 IMPROVE: Added a sample text to the main page
    #43

--- lib/pages/fullstack/bank_app/bank_main.dart
+++ lib/pages/fullstack/bank_app/bank_main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class FlutterBankMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text('main page'),
+      ),
+    );
   }
 }
