f79fad55432325a9060d464c33ca04daf3eb8f14
GLinBoy <hojjatabedie@gmail.com>
2023-06-19 19:51:35 +0200

    👌 IMPROVE: Updated structure of login service
    #43

--- lib/pages/fullstack/bank_login/login_service.dart
+++ lib/pages/fullstack/bank_login/login_service.dart
@@ -1,3 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginService extends ChangeNotifier {}
+class LoginService extends ChangeNotifier {
+  String _userId = '';
+
+  String getUserId() {
+    return _userId;
+  }
+
+  Future<bool> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential credentials =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      _userId = credentials.user!.uid;
+      return true;
+    } on FirebaseAuthException catch (ex) {
+      return false;
+    }
+  }
+}
