3240a158f05978467a3c977722e25cc47ddfd145
GLinBoy <hojjatabedie@gmail.com>
2023-06-20 20:22:46 +0200

    👌 IMPROVE: Configured firebase
    #43

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+    ),
+  );
+
   runApp(
     const MaterialApp(
       title: 'Flutter Demo',
