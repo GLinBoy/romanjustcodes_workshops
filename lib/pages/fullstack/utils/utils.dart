4cd64220feb7ec60eb6ec73c1420f4abe4829dbc
GLinBoy <hojjatabedie@gmail.com>
2023-06-20 20:27:15 +0200

    👌 IMPROVE: Added email validation method to utils
    #43

--- lib/pages/fullstack/utils/utils.dart
+++ lib/pages/fullstack/utils/utils.dart
@@ -2,4 +2,14 @@ import 'package:flutter/cupertino.dart';
 
 class Utils {
   static const Color mainThemeColor = Color(0xFF8700C3);
+
+  static bool validateEmail(String? value) {
+    String pattern =
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?)*$";
+    RegExp regex = RegExp(pattern);
+
+    return (value != null || value!.isNotEmpty || regex.hasMatch(value));
+  }
 }
