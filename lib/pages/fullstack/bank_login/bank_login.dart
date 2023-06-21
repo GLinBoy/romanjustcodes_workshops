0cf25e911d2676ceec009c6b70cffb53cb663104
GLinBoy <hojjatabedie@gmail.com>
2023-06-21 19:53:09 +0200

    👌 IMPROVE: Added dispose method to clean up
    #43

--- lib/pages/fullstack/bank_login/bank_login.dart
+++ lib/pages/fullstack/bank_login/bank_login.dart
@@ -202,6 +202,13 @@ class FlutterBankLoginState extends State<FlutterBankLogin> {
     );
   }
 
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   bool validateEmailAndPassword() {
     return usernameController.value.text.isNotEmpty &&
         passwordController.value.text.isNotEmpty &&
