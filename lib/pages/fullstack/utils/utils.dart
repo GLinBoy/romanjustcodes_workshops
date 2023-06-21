4135210d8b090a44c9d694e0a563646c324720b5
GLinBoy <hojjatabedie@gmail.com>
2023-06-21 20:42:01 +0200

    👌 IMPROVE: Added generated input field for registration
    #43

--- lib/pages/fullstack/utils/utils.dart
+++ lib/pages/fullstack/utils/utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Utils {
   static const Color mainThemeColor = Color(0xFF8700C3);
@@ -11,5 +12,39 @@ class Utils {
     RegExp regex = RegExp(pattern);
 
     return (value != null || value!.isNotEmpty || regex.hasMatch(value));
+  }
+
+  static Widget generateInputField(
+    String hintText,
+    IconData iconData,
+    TextEditingController controller,
+    bool isPasswordField,
+    Function onChanged,
+  ) {
+    return Container(
+        padding: const EdgeInsets.all(5),
+        margin: const EdgeInsets.only(bottom: 20),
+        decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(50)),
+        child: TextField(
+          onChanged: (text) {
+            onChanged(text);
+          },
+          obscureText: isPasswordField,
+          obscuringCharacter: "*",
+          decoration: InputDecoration(
+              prefixIcon: Icon(iconData, color: Utils.mainThemeColor),
+              border: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              errorBorder: InputBorder.none,
+              disabledBorder: InputBorder.none,
+              contentPadding:
+                  EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+              hintText: hintText),
+          controller: controller,
+          style: const TextStyle(fontSize: 16),
+        ));
   }
 }
