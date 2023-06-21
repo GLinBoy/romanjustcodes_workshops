3a7aa833a6919d017faee92405d95c3f8e335c63
GLinBoy <hojjatabedie@gmail.com>
2023-06-21 19:57:54 +0200

    📦 NEW: Added registration class
    #43

---
+++ lib/pages/fullstack/bank_login/bank_registration.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlutterAccountRegistration extends StatefulWidget {
+  @override
+  FlutterAccountRegistrationState createState() =>
+      FlutterAccountRegistrationState();
+}
+
+class FlutterAccountRegistrationState
+    extends State<FlutterAccountRegistration> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
