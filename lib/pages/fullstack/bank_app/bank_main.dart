22f228a61daa1407ed58a5d79aa8d049c744826b
GLinBoy <hojjatabedie@gmail.com>
2023-12-31 16:04:11 +0100

    📦 NEW: Added an account class
    #44

--- lib/pages/fullstack/bank_app/bank_main.dart
+++ lib/pages/fullstack/bank_app/bank_main.dart
@@ -10,3 +10,21 @@ class FlutterBankMain extends StatelessWidget {
     );
   }
 }
+
+class Account {
+  String? id;
+  String? type;
+  String? accountNumber;
+  double? balance;
+
+  Account({this.id, this.type, this.accountNumber, this.balance});
+
+  factory Account.fromJson(Map<String, dynamic> json, String docId) {
+    return Account(
+      id: docId,
+      type: json['type'],
+      accountNumber: json['account_number'],
+      balance: json['balance'],
+    );
+  }
+}
