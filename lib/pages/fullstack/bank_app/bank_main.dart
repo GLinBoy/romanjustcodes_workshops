72797a52a83a9efab1a35e83b789c17120ed9af4
GLinBoy <hojjatabedie@gmail.com>
2023-12-31 17:30:22 +0100

    👌 IMPROVE: Fetch user data from Firebase
    #44

--- lib/pages/fullstack/bank_app/bank_main.dart
+++ lib/pages/fullstack/bank_app/bank_main.dart
@@ -4,13 +4,96 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:romanjustcodes_workshops/pages/fullstack/bank_login/login_service.dart';
+import 'package:romanjustcodes_workshops/pages/fullstack/utils/utils.dart';
 
 class FlutterBankMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('main page'),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Utils.mainThemeColor),
+        backgroundColor: Colors.transparent,
+        title: const Icon(
+          Icons.savings,
+          color: Utils.mainThemeColor,
+          size: 40,
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                Icon(
+                  Icons.account_balance_wallet,
+                  color: Utils.mainThemeColor,
+                  size: 30,
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'My Accounts',
+                  style: TextStyle(
+                    color: Utils.mainThemeColor,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Consumer<FlutterBankService>(
+                builder: (context, bankService, child) {
+                  return FutureBuilder(
+                    future: bankService.getAccounts(context),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.connectionState != ConnectionState.done ||
+                          !snapshot.hasData) {
+                        return Text('Loading');
+                      }
+
+                      List<Account> accounts = snapshot.data as List<Account>;
+                      if (accounts.isEmpty) {
+                        return Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(
+                                Icons.account_balance_wallet,
+                                color: Utils.mainThemeColor,
+                                size: 50,
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                'You don\'t have any accounts\nassociated with your profile.',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Utils.mainThemeColor,
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      }
+
+                      return ListView.builder(
+                        itemCount: accounts.length,
+                        itemBuilder: (context, index) {
+                          var acct = accounts[index];
+                          return Text(acct.type!);
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
