cdb569bd060b168eca8617dc02d86bc8b2b59656
GLinBoy <hojjatabedie@gmail.com>
2024-01-01 11:50:41 +0100

    👌 IMPROVE: Added bottom bar widget
    #44

--- lib/pages/fullstack/bank_app/bank_main.dart
+++ lib/pages/fullstack/bank_app/bank_main.dart
@@ -96,6 +96,7 @@ class FlutterBankMain extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: FlutterBankBottomBar(),
     );
   }
 }
@@ -275,6 +276,65 @@ class FlutterBankBottomBarItem {
 class FlutterBankBottomBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var bottomItems = Utils.getBottomBarItems();
+
+    return Container(
+      height: 100,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Utils.mainThemeColor.withOpacity(0.05),
+            blurRadius: 10,
+            offset: Offset.zero,
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: List.generate(
+          bottomItems.length,
+          (index) {
+            FlutterBankBottomBarItem bottomItem = bottomItems[index];
+
+            return Material(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(10),
+              clipBehavior: Clip.antiAlias,
+              child: InkWell(
+                highlightColor: Utils.mainThemeColor.withOpacity(0.2),
+                splashColor: Utils.mainThemeColor.withOpacity(0.1),
+                onTap: () {
+                  bottomItem.action!();
+                },
+                child: Container(
+                  constraints: BoxConstraints(minWidth: 80),
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        bottomItem.icon,
+                        color: Utils.mainThemeColor,
+                        size: 20,
+                      ),
+                      Text(
+                        bottomItem.label!,
+                        style: TextStyle(
+                          color: Utils.mainThemeColor,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
