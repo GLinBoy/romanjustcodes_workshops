93d037cb14de7774779ead8be1a7afe63d8ae2b4
GLinBoy <hojjatabedie@gmail.com>
2024-01-02 19:52:38 +0100

    👌 IMPROVE: Added DepositService
    #45

--- lib/pages/fullstack/bank_app/bank_main.dart
+++ lib/pages/fullstack/bank_app/bank_main.dart
@@ -379,3 +379,21 @@ class FlutterBankDrawer extends StatelessWidget {
     );
   }
 }
+
+class DepositService extends ChangeNotifier {
+  double amountToDeposit = 0;
+
+  void setAmountToDeposit(double amount) {
+    amountToDeposit = amount;
+    notifyListeners();
+  }
+
+  void resetDepositService() {
+    amountToDeposit = 0;
+    notifyListeners();
+  }
+
+  bool checkAmountToDeposit() {
+    return amountToDeposit > 0;
+  }
+}
