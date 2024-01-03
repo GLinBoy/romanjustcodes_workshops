472ccaecbeac4f517f34f284af706c282f150288
GLinBoy <hojjatabedie@gmail.com>
2024-01-03 20:59:11 +0100

    👌 IMPROVE: Enhanced AccountActionCard widget
    #45

--- lib/pages/fullstack/bank_app/bank_main.dart
+++ lib/pages/fullstack/bank_app/bank_main.dart
@@ -524,8 +524,62 @@ class FlutterBankError extends StatelessWidget {
 }
 
 class AccountActionCard extends StatelessWidget {
+  final List<Account>? accounts;
+  final Account? selectedAccount;
+
+  const AccountActionCard({this.accounts, this.selectedAccount});
+
   @override
   Widget build(BuildContext context) {
-    return Row();
+    FlutterBankService bankService =
+        Provider.of<FlutterBankService>(context, listen: false);
+
+    return Row(
+      children: List.generate(
+        accounts!.length,
+        (index) {
+          var currentAccount = accounts![index];
+          return Expanded(
+            child: GestureDetector(
+              onTap: () {
+                bankService.setSelectedAccount(currentAccount);
+              },
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 20,
+                      offset: const Offset(0.0, 5.0),
+                    ),
+                  ],
+                  border: Border.all(
+                    width: 5,
+                    color: selectedAccount != null &&
+                            selectedAccount!.id == currentAccount.id
+                        ? Utils.mainThemeColor
+                        : Colors.transparent,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${currentAccount.type!.toUpperCase()} ACCT',
+                      style: TextStyle(color: Utils.mainThemeColor),
+                    ),
+                    Text(currentAccount.accountNumber!),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
