f7c94d59f5cd53883022a6d812c63b1d1229b221
GLinBoy <hojjatabedie@gmail.com>
2024-01-07 13:02:34 +0100

    👌 IMPROVE: Added Expense class
    #47

--- lib/pages/fullstack/bank_app/bank_main.dart
+++ lib/pages/fullstack/bank_app/bank_main.dart
@@ -970,3 +970,20 @@ class AccountWithdrawalSlider extends StatelessWidget {
     );
   }
 }
+
+class Expense {
+  String? name;
+  double? amount;
+  String? timestamp;
+  String? id;
+
+  Expense({this.id, this.name, this.amount, this.timestamp});
+
+  factory Expense.fromJson(Map<String, dynamic> json, String docId) {
+    return Expense(
+        id: docId,
+        name: json['name'],
+        amount: json['amount'],
+        timestamp: json['timestamp']);
+  }
+}
