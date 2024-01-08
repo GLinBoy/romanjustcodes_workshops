9b3393612f5d555220fcb6b1f8483937299d3bcc
GLinBoy <hojjatabedie@gmail.com>
2024-01-08 20:27:19 +0100

    👌 IMPROVE: Enhanced BankNoExpenses widget body
    #47

--- lib/pages/fullstack/bank_app/bank_main.dart
+++ lib/pages/fullstack/bank_app/bank_main.dart
@@ -1056,6 +1056,20 @@ class FlutterBankExpenses extends StatelessWidget {
 class FlutterBankNoExpenses extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Icon(Icons.payments, color: Utils.mainThemeColor, size: 50),
+          SizedBox(height: 20),
+          Text(
+            'There are no expenses\nat the moment.',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Utils.mainThemeColor),
+          ),
+        ],
+      ),
+    );
   }
 }
