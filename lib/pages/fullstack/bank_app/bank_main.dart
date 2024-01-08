df994cb3b5ea11e36a4daaab0a625a568c6027c8
GLinBoy <hojjatabedie@gmail.com>
2024-01-08 21:07:33 +0100

    👌 IMPROVE: Enhanced ExpenseCard body
    #47

--- lib/pages/fullstack/bank_app/bank_main.dart
+++ lib/pages/fullstack/bank_app/bank_main.dart
@@ -1111,6 +1111,60 @@ class ExpenseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 15,
+            offset: const Offset(0.0, 5.0),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                expense!.name!,
+                textAlign: TextAlign.left,
+                style: const TextStyle(fontSize: 12),
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.monetization_on,
+                      color: Utils.mainThemeColor, size: 20),
+                  Text(
+                    '\$${expense!.amount!.toStringAsFixed(2)}',
+                    style: const TextStyle(color: Colors.black, fontSize: 25),
+                  ),
+                ],
+              ),
+              Text(
+                DateFormat.yMd()
+                    .add_jm()
+                    .format(DateTime.parse(expense!.timestamp!)),
+              ),
+            ],
+          ),
+          ClipOval(
+            child: Material(
+              color: Colors.transparent,
+              child: IconButton(
+                icon: const Icon(Icons.delete_forever, color: Colors.grey),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
